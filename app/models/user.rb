96c31459444bb59044aa17e6105037a15b63cf45
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-10-23 14:21:25 +0000

    quicksave

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,5 +28,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  has_many: :own_photos, :class_name: "Photo", foreign_key: "owner_id" 
+  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id" 
 end
